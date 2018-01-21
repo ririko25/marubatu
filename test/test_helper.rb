761cee7c99f677470471cfbd932148f7292aef44
ririko <ririkokanikama25@yahoo.co.jp>
2018-01-21 13:31:37 +0900

    reporters追加したよ

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "marubatu"
 
+require "minitest"
 require "minitest/autorun"
+
+require "minitest/reporters"
+Minitest::Reporters.use!
